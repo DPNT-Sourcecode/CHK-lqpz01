e5749c5d0aaf8da971ea68858cc573a6879c6169
sbx_user1051 <sbx_user1051@169.254.113.157>
2022-05-18 04:52:35 +0000

    Wed May 18 04:52:35 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,8 +4,37 @@
 # skus = unicode string
 
 class Checkout:
-    pass
+    """Class to interact with price table
+    Example Table:
+    +------+-------+----------------+
+    | Item | Price | Special offers |
+    +------+-------+----------------+
+    | A    | 50    | 3A for 130     |
+    | B    | 30    | 2B for 45      |
+    | C    | 20    |                |
+    | D    | 15    |                |
+    +------+-------+----------------+
+    """
+    
+    def __init__(self, price_table:str):
+        price_table = price_table.strip()
+        # prices are stored in a dict
+        self.prices: dict[str, tuple[int, int]] = {}
+        for line in price_table.splitlines()[3:-1]:
+            item, price, offer = line.split('|')
+            item = item.strip()
+            price = int(price.strip())
+            offer = offer.split(' for ')
+            offer_count = int(offer[0].strip()[:-1])
+            offer_price = int(offer[1].strip())
+            self.prices[item] = {1: price, offer_count: offer_price}
+            
+    def get_price(self, skus:str) -> int:
+        """Return total price for all SKUs"""
+        skus = skus
+            
 
 def checkout(skus):
     raise NotImplementedError()
+
 
