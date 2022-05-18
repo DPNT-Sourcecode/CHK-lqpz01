eec720199ded24430c1e002a065e56be9aa5e5e5
sbx_user1051 <sbx_user1051@169.254.113.157>
2022-05-18 04:25:03 +0000

    Wed May 18 04:25:03 UTC 2022

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -4,11 +4,13 @@ import unittest
 class TestHLO(unittest.TestCase):
     
     def test_hello(self):
-        self.assertEqual(hello_solution.hello('Mr. X'), 'Hello, World!')
+        self.assertEqual(hello_solution.hello(''), 'Hello, World!')
         
-   
+    def test_name_input(self):
+        self.assertEqual(hello_solution.hello('John'), 'Hello, John!')
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
