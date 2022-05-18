9f37bdfb1ac8f9e6fc8bf853d78f024863d2984a
sbx_user1051 <sbx_user1051@169.254.0.185>
2022-05-18 10:44:14 +0000

    Wed May 18 10:44:14 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -83,7 +83,49 @@ class TestCheckoutThird(unittest.TestCase):
         self.assertEqual(self.checkout.get_price('FFFF'), 30)
         self.assertEqual(self.checkout.get_price('FFFFF'), 40)
         
+class TestCheckoutFourth(unittest.TestCase):
     
+    def setUp(self):
+        price_table = """
+        +------+-------+------------------------+
+        | Item | Price | Special offers         |
+        +------+-------+------------------------+
+        | A    | 50    | 3A for 130, 5A for 200 |
+        | B    | 30    | 2B for 45              |
+        | C    | 20    |                        |
+        | D    | 15    |                        |
+        | E    | 40    | 2E get one B free      |
+        | F    | 10    | 2F get one F free      |
+        | G    | 20    |                        |
+        | H    | 10    | 5H for 45, 10H for 80  |
+        | I    | 35    |                        |
+        | J    | 60    |                        |
+        | K    | 80    | 2K for 150             |
+        | L    | 90    |                        |
+        | M    | 15    |                        |
+        | N    | 40    | 3N get one M free      |
+        | O    | 10    |                        |
+        | P    | 50    | 5P for 200             |
+        | Q    | 30    | 3Q for 80              |
+        | R    | 50    | 3R get one Q free      |
+        | S    | 30    |                        |
+        | T    | 20    |                        |
+        | U    | 40    | 3U get one U free      |
+        | V    | 50    | 2V for 90, 3V for 130  |
+        | W    | 20    |                        |
+        | X    | 90    |                        |
+        | Y    | 10    |                        |
+        | Z    | 50    |                        |
+        +------+-------+------------------------+
+        """
+        
+        self.checkout = checkout_solution.Checkout(price_table)
+        
+    
+    def test_larger_list(self):
+        self.assertEqual(self.checkout.get_price('H'*11), 80+10)
+        self.assertEqual(self.checkout.get_price('H'*11+'U'*4), 90+120)
+        
   
 class TestFavour(unittest.TestCase):
     
